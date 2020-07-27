7f7eb089c8a344843f88287462ebdde423f46c3d
ananditabhatt <anay_mahajan@infosys.com>
2020-07-27 14:20:20 +1000

    New React create project

--- React-InterviewQuestions/foreground.sh
+++ React-InterviewQuestions/foreground.sh
@@ -1,6 +1,6 @@
 echo "Setting up React project for pratice"
 
 npm install create-react-app -g
-create-react-app react-Interview-practice --scripts-version 1.1.5
-cd react-Interview-practice
+create-react-app react-interview-practice --scripts-version 1.1.5
+cd react-interview-practice
 npm start
